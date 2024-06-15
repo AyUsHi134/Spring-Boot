df992c4401b14b0deba3c53ebbfe69bcf9863ef4
AyUsHi134 <singhayushi9369@gmail.com>
2024-06-15 23:31:58 +0530

    Dependency Injection

---
+++ 02-Spring-core-coachRoutine/coachRoutine/coachRoutine/src/test/java/com/springcore/coachRoutine/CoachRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcore.coachRoutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
