073c036fe5abaf49c8ba2bb6585a07c497431b9e
AyUsHi134 <singhayushi9369@gmail.com>
2024-06-19 23:45:21 +0530

    Dependency Injection

--- 02-Spring-core-coachRoutine/coachRoutine/coachRoutine/src/main/java/com/springcore/coachRoutine/CricketCoach.java
+++ 02-Spring-core-coachRoutine/coachRoutine/coachRoutine/src/main/java/com/springcore/coachRoutine/CricketCoach.java
@@ -1,11 +1,13 @@
 package com.springcore.coachRoutine;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Component
 public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout() {
-        return "Practice ";
+        return "Practice fast bowling for 15 minutes";
     }
+
 }
